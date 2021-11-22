e4ea2412a0780f562ee6bad4cec4830bb5ada7a9
jisuz <jisuz1997@gmail.com>
2021-11-22 21:18:22 +0200

    homework 2.1

---
+++ app/code/Jisuz/TestObserver/Observer/LogPageHtml.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jisuz\TestObserver\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class LogPageHtml implements ObserverInterface
+{
+    private $logger;
+
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+    }
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $response = $observer->getEvent()->getData('response');
+        $this->logger->debug($response->getBody());
+    }
+}
