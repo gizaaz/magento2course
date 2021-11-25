9db35b1bc296121a6abba0ac2dc401018e0ca7a8
jisuz <jisuz1997@gmail.com>
2021-11-25 22:41:41 +0200

    homework 2.6

---
+++ app/code/Jisuz/TestRouting/Controller/Product/View.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jisuz\TestRouting\Controller\Product;
+
+
+class View extends \Magento\Catalog\Controller\Product\View
+{
+    private $customerSession;
+
+    private $redirectFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Catalog\Helper\Product\View $viewHelper,
+        \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
+    ) {
+        $this->customerSession = $customerSession;
+        $this->redirectFactory = $redirectFactory;
+        parent::__construct($context, $viewHelper, $resultForwardFactory, $resultPageFactory);
+    }
+
+    public function execute()
+    {
+        if (!$this->customerSession->isLoggedIn()) {
+            return $this->redirectFactory->create()->setPath('customer/account/login');
+        }
+
+        return parent::execute();
+    }
+}
