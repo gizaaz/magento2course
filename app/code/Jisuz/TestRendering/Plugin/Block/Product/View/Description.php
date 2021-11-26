861292876688c0fcca4a6ea50c48dda7938b3f36
jisuz <jisuz1997@gmail.com>
2021-11-26 23:28:32 +0200

    homework 3.4

---
+++ app/code/Jisuz/TestRendering/Plugin/Block/Product/View/Description.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jisuz\TestRendering\Plugin\Block\Product\View;
+
+class Description extends \Magento\Catalog\Block\Product\View\Description
+{
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+        $subject->getProduct()->setDescription('Test description');
+    }
+}
