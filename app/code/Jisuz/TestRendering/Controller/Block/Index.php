b8a63676510b3a7da2ebc91ffc017005e263d6c5
jisuz <jisuz1997@gmail.com>
2021-11-26 22:43:58 +0200

    homework 3.3

--- app/code/Jisuz/TestRendering/Controller/Block/Index.php
+++ app/code/Jisuz/TestRendering/Controller/Block/Index.php
@@ -8,16 +8,21 @@ class Index implements \Magento\Framework\App\ActionInterface
 {
     private $layoutFactory;
 
+    private $resultRawFactory;
+
     public function __construct(
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\Controller\ResultFactory $resultRawFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
     }
 
     public function execute()
     {
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock('Jisuz\TestRendering\Block\Test');
-        echo $block->toHtml();
+        $resultRaw = $this->resultRawFactory->create($this->resultRawFactory::TYPE_RAW);
+        return $resultRaw->setContents($block->toHtml());
     }
 }
