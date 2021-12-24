b1de29fdf90df35b263ab4d9ed49b7f572cd8e2b
jisuz <jisuz1997@gmail.com>
2021-12-24 16:24:37 +0200

    homework 4.2

---
+++ app/code/Jisuz/TestJs/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/catalog-add-to-cart': {
+                'Jisuz_TestJs/js/catalog-add-to-cart/mixin': true
+            }
+        }
+    }
+}
